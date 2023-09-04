66f08abb95f29fb1191ab8da8ae4c719df39ea44
Yakin Nikita <yakin95@gmail.com>
2023-09-04 22:18:03 +0500

    fix(components): yandex-metrika-informer

--- src/runtime/components/yandex-metrika-informer.vue
+++ src/runtime/components/yandex-metrika-informer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useYandexMetrika } from './composables/useYandexMetrika';
+import { useYandexMetrika } from '../composables/useYandexMetrika';
 const { id } = useYandexMetrika();
 </script>
 
